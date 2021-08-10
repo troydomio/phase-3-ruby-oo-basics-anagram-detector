d9320f8848ab5a2fe285c705ac2a61028f2afe00
Troy <troyakins99@yahoo.com>
2021-08-10 16:40:32 -0500

    commit tree 577f478b9ee185862e9cc9c4506afdf8b6642d24

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+    attr_reader :new
+    attr_writer :match
+
+    def initialize(name)
+        @new = name
+    end
+
+    def match (match)
+    #   @match = []
+
+      match.filter do |words|
+        words.chars.sort == @new.chars.sort
+      end
+
+    end
+end
